8355824eea9ab53ce4f1ca6b9054dfa193f4fe13
Nick Fraser <icanlosh@gmail.com>
2023-08-08 13:51:28 +0100

    [test] Added basic sparse linear neq instantiation test

--- tests/logicnets/test_nn.py
+++ tests/logicnets/test_nn.py
@@ -1,9 +1,15 @@
 
+from brevitas.nn import QuantReLU
+from brevitas.core.quant import QuantType
+from brevitas.core.scaling import ScalingImplType
+
+from logicnets.quant import QuantBrevitasActivation
 from logicnets.nn import    ScalarBiasScale, \
                             ScalarScaleBias, \
                             DenseMask2D, \
                             RandomFixedSparsityMask2D, \
-                            SparseLinear
+                            SparseLinear, \
+                            SparseLinearNeq
 
 def test_instantiate_scalar_bias_scale():
     m = ScalarBiasScale()
@@ -25,4 +31,13 @@ def test_instantiate_sparse_linear():
     mask = DenseMask2D(1, 1)
     m = SparseLinear(1, 1, mask)
     assert isinstance(m, SparseLinear)
+
+def test_instantiate_sparse_linear_neq():
+    qi = QuantReLU(bit_width=2, max_val=1.0, quant_type=QuantType.INT, scaling_impl_type=ScalingImplType.PARAMETER)
+    qo = QuantReLU(bit_width=2, max_val=1.0, quant_type=QuantType.INT, scaling_impl_type=ScalingImplType.PARAMETER)
+    qbi = QuantBrevitasActivation(qi)
+    qbo = QuantBrevitasActivation(qo)
+    mask = DenseMask2D(1, 1)
+    m = SparseLinearNeq(1, 1, qbi, qbo, sparse_linear_kws={'mask': mask})
+    assert isinstance(m, SparseLinearNeq)
 

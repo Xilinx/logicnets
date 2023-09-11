81717ed5174590294f993ff27dff5022f5aad411
Nick Fraser <icanlosh@gmail.com>
2023-09-11 15:28:22 +0100

    [test] Upgraded quant test

--- tests/logicnets/test_quant.py
+++ tests/logicnets/test_quant.py
@@ -10,16 +10,17 @@ import torch
 from brevitas.nn import QuantReLU, QuantHardTanh
 from brevitas.core.quant import QuantType
 from brevitas.core.scaling import ScalingImplType
+from brevitas.core.restrict_val import RestrictValueType
 
 from logicnets.quant import QuantBrevitasActivation
 
-from tests.logicnets.util import gen_ndarray, gen_bit_width
+from tests.logicnets.util import gen_ndarray, gen_bit_width, gen_power_of_two
 
 MIN_DIM=1
 MAX_DIM=10
 MIN_DIMS=1
 MAX_DIMS=5
-MIN_SCALE_FACTOR=1e-8
+SCALING_MIN_VAL=1e-8
 
 def test_instantiate_quant_brevitas_activation():
     qa = QuantReLU(bit_width=2, max_val=1.0, quant_type=QuantType.INT, scaling_impl_type=ScalingImplType.PARAMETER)
@@ -27,27 +28,37 @@ def test_instantiate_quant_brevitas_activation():
     assert isinstance(m, QuantBrevitasActivation)
 
 @given( x_np=gen_ndarray(min_dims=MIN_DIMS, max_dims=MAX_DIMS, min_side=MIN_DIM, max_side=MAX_DIM),
-        bit_width=gen_bit_width(),
+        bit_width=gen_bit_width(min_val=2),
+        scale_factor=gen_power_of_two(min_val=-1, max_val=0),
         gpu=st.booleans(),
+        float_scale=st.booleans(),
         )
-@example(bit_width=2, x_np=np.array([0.25], dtype=np.float32), gpu=False)
-@example(bit_width=2, x_np=np.array([0.25], dtype=np.float32), gpu=True)
-@example(bit_width=2, x_np=np.array([0.2500000298023223876953125], dtype=np.float32), gpu=False)
-@example(bit_width=2, x_np=np.array([0.2500000298023223876953125], dtype=np.float32), gpu=True)
-@example(bit_width=2, x_np=np.array([0.24999998509883880615234375], dtype=np.float32), gpu=False)
-@example(bit_width=2, x_np=np.array([0.24999998509883880615234375], dtype=np.float32), gpu=True)
+@example(bit_width=2, x_np=np.array([0.25], dtype=np.float32), scale_factor=0.5, gpu=False, float_scale=False)
+@example(bit_width=2, x_np=np.array([0.25], dtype=np.float32), scale_factor=0.5, gpu=True, float_scale=False)
+@example(bit_width=2, x_np=np.array([0.2500000298023223876953125], dtype=np.float32), scale_factor=0.5, gpu=False, float_scale=False)
+@example(bit_width=2, x_np=np.array([0.2500000298023223876953125], dtype=np.float32), scale_factor=0.5, gpu=True, float_scale=False)
+@example(bit_width=2, x_np=np.array([0.24999998509883880615234375], dtype=np.float32), scale_factor=0.5, gpu=False, float_scale=False)
+@example(bit_width=2, x_np=np.array([0.24999998509883880615234375], dtype=np.float32), scale_factor=0.5, gpu=True, float_scale=False)
 @torch.no_grad()
-def test_forward_quant_relu_brevitas_activation(x_np, bit_width, gpu, fetch_device, fetch_dtype, fetch_result, allclose):
+def test_forward_quant_relu_brevitas_activation(x_np, bit_width, scale_factor, gpu, float_scale, fetch_device, fetch_dtype, fetch_result, allexact, allclose):
+    if float_scale:
+        validate=allclose
+        restrict_scaling_type=RestrictValueType.FP
+    else:
+        validate=allexact
+        restrict_scaling_type=RestrictValueType.POWER_OF_TWO
     device = fetch_device(gpu)
     dtype = fetch_dtype(x_np.dtype)
     x = torch.from_numpy(x_np).to(device)
-    qa = QuantReLU(bit_width=bit_width, max_val=2**(bit_width-1) - 0.5, quant_type=QuantType.INT, scaling_impl_type=ScalingImplType.PARAMETER)
+    qa = QuantReLU(bit_width=bit_width, max_val=scale_factor*(2**bit_width-1), quant_type=QuantType.INT, scaling_impl_type=ScalingImplType.PARAMETER, restrict_scaling_type=restrict_scaling_type, scaling_min_val=SCALING_MIN_VAL)
     module = QuantBrevitasActivation(qa).to(device, dtype)
     module.eval()
-    scale_factor, bits = module.get_scale_factor_bits()
+    sf, bits = module.get_scale_factor_bits()
     assert bits == bit_width
-    assert allclose(fetch_result(scale_factor), np.array(0.5, dtype=x_np.dtype))
+    if not validate(fetch_result(sf), scale_factor):
+        print(f"{fetch_result(sf):.16e}, {scale_factor:.16e}")
+    assert validate(fetch_result(sf), scale_factor)
     y_test = fetch_result(qa(x))
     y_ref = fetch_result(torch.clamp(torch.round(x / scale_factor), 0, (2**bit_width - 1))*scale_factor)
-    assert allclose(y_test, y_ref)
+    assert validate(y_test, y_ref)
 
--- tests/logicnets/util.py
+++ tests/logicnets/util.py
@@ -12,6 +12,11 @@ def gen_ndarray(**shape_kws):
 def gen_seed():
     return st.integers(min_value=0, max_value=2**32 - 1)
 
-def gen_bit_width():
-    return st.integers(min_value=1, max_value=8)
+def gen_bit_width(min_val=1, max_val=8):
+    return st.integers(min_value=min_val, max_value=max_val)
+
+@st.composite
+def gen_power_of_two(draw, min_val=-8, max_val=7):
+    power = draw(st.integers(min_value=min_val, max_value=max_val))
+    return float(2**power)
 
